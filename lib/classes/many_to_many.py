062f190d5ab90aaefa182db2f4e289396adb8a4d
Matthew <matthew.stewart827@gmail.com>
2023-09-20 15:34:14 -0500

    completed all properties for all classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,6 +1,15 @@
 class Game:
     def __init__(self, title):
         self.title = title
+        
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and not hasattr(self, "title") and len(title) > 0:
+            self._title = title
 
     def results(self):
         pass
@@ -14,6 +23,15 @@ class Game:
 class Player:
     def __init__(self, username):
         self.username = username
+        
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
 
     def results(self):
         pass
@@ -28,7 +46,19 @@ class Player:
         pass
 
 class Result:
+    all = []
+    
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        Result.all.append(self)
+    
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if isinstance(score, int) and 1 <= score <= 5000 and not hasattr(self, "score"):
+            self._score = score
